350073c47259b4f83c7847377c771dedd446f510
2muni <relesinc@gmail.com>
2019-06-14 14:46:21 +0900

    JWT Authentication via Passport

---
+++ src/passport.js
@@ -0,0 +1,41 @@
+import passport from "passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { prisma } from "../generated/prisma-client";
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+};
+
+const verifyUser = async (payload, done) => {
+  try {
+    const user = await prisma.user({ id: payload.id });
+    if (user !== null) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (e) {
+    return done(e, false);
+  }
+};
+
+export const authenticateJwt = (req, res, next) =>
+  passport.authenticate("jwt", { session: false }, (error, user) => {
+    if (user) {
+      req.user = user;
+    }
+    next();
+  })(req, res, next);
+
+passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
+
+/**
+ * express middleware at server.js
+ * -> authenticateJwt()
+ * -> jwt() // use Strategy to get JWT Token
+ * -> verifyUser(JWT: payload)
+ * -> authenticateJwt's callback func()
+ * -> append user prop to HTTP request
+ */
